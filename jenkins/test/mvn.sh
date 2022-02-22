5bb21e6d8d4795fed0c287c619c04e43a1453d69
kapil <kapil.kumaria@gmail.com>
2022-02-22 14:10:01 +0000

    fixes workspace path for job execution

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,6 @@ echo "***************************************************"
 echo "*************** Testing the Code ******************"
 echo "***************************************************"
 
-docker run -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
+WORKSPACE=/home/centos/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
