38d21984eed6f60137e371baae640efec4e47953
kapil <kapil.kumaria@gmail.com>
2022-02-23 05:28:48 +0000

    modified workspaces for scripts

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,8 +4,10 @@ echo "***********************************************"
 echo "*************** Building Jar ******************"
 echo "***********************************************"
 
-# WORKSPACE=/home/centos/jenkins-data/jenkins_home/workspace/new-pipeline
+WORKSPACE=/home/centos/jenkins-data/jenkins_home/workspace/pipeline-project
 
 # docker run -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
+#docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,6 +6,9 @@ echo "***************************************************"
 
 #WORKSPACE=/home/centos/jenkins-data/jenkins_home/workspace/new-pipeline
 
+WORKSPACE=/home/centos/jenkins-data/jenkins_home/workspace/pipeline-project
+
+
 docker run -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
 
-docker run -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
+#docker run -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5-alpine "$@"
